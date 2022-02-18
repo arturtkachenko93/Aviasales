e9ec6008d323d95ee70a0d2542e871f3506615eb
Artur T <artur_terner@mail.ru>
2022-02-19 01:58:51 +0300

    the layout of the header and the form is completed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Form } from '../Form';
+import classes from './App.module.scss';
+import Logo from './Logo.svg';
 
 const App = () => {
-  return <div>hello</div>;
+  return (
+    <>
+      <header className={classes.header}>
+        <img src={Logo} alt="logo" />
+      </header>
+      <div className={classes.wrap}>
+        <Form />
+        <div>dsd</div>
+      </div>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import classes from './Form.module.scss';
+
+const Form = () => {
+  return (
+    <form className={classes.form}>
+      <fieldset className={classes.wrapper}>
+        <legend className={classes.title}>Количество пересадок</legend>
+        <ul className={classes['form-list']}>
+          <li>
+            <label className={classes.label}>
+              <input className="visually-hidden" type="checkbox" />
+              <span className={classes.checkbox} />
+              Все
+            </label>
+          </li>
+          <li>
+            <label className={classes.label}>
+              <input cl className="visually-hidden" type="checkbox" />
+              <span className={classes.checkbox} />
+              Без пересадок
+            </label>
+          </li>
+          <li>
+            <label className={classes.label}>
+              <input className="visually-hidden" type="checkbox" />
+              <span className={classes.checkbox} />1 пересадка
+            </label>
+          </li>
+          <li>
+            <label className={classes.label}>
+              <input className="visually-hidden" type="checkbox" />
+              <span className={classes.checkbox} />2 пересадки
+            </label>
+          </li>
+          <li>
+            <label className={classes.label}>
+              <input className="visually-hidden" type="checkbox" />
+              <span className={classes.checkbox} />3 пересадки
+            </label>
+          </li>
+        </ul>
+      </fieldset>
+    </form>
+  );
+};
+
+export default Form;
